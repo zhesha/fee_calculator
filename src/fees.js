9a192ec3fffbee43908c1367bc97dae8ec913c92
zhesha <savzhe@gmail.com>
2023-03-27 14:35:57 +0300

    added calculation for CashIn and Juridical CashOut

--- src/fees.js
+++ src/fees.js
@@ -1,5 +1,59 @@
 function calculateFees(input) {
-  console.log(input);
+  if (!Array.isArray(input)) {
+    throw new Error("Input data must be an array with transactions");
+  }
+  const feesList = input.map((transaction) => calculateFeeForTransaction(transaction));
+  return feesList;
+}
+
+function calculateFeeForTransaction(transaction) {
+  const { date, user_id: userId, user_type: userType, type, operation } = transaction;
+  if (!isValidTransaction(transaction)) {
+    throw new Error(`Invalid transaction: ${JSON.stringify(transaction)}`);
+  }
+  if (type === "cash_in") {
+    return calculateCashInFee(operation.amount);
+  }
+  if (type === "cash_out") {
+    if (userType === "natural") {
+      return calculateNaturalCashOutFee(date, userId, operation.amount);
+    }
+    if (userType === "juridical") {
+      return calculateJuridicalCashOutFee(operation.amount);
+    }
+  }
+};
+
+function isValidTransaction(transaction) {
+  const { date, user_id: userId, user_type: userType, type, operation } = transaction;
+  if (typeof date !== "string" || typeof userId !== "number" || typeof userType !== "string" || typeof type !== "string" || !operation) {
+    return false;
+  }
+  if (userType !== "natural" && userType !== "juridical") {
+    return false;
+  }
+  if (type !== "cash_in" && type !== "cash_out") {
+    return false;
+  }
+  if (!operation.amount || operation.amount < 0) {
+    return false;
+  }
+  return true;
+}
+
+function calculateCashInFee(amount) {
+  const fee = amount * 0.0003;
+  return fee > 5 ? 5 : fee;
+}
+
+function calculateNaturalCashOutFee(date, userId, amount) {
+  const fee = amount * 0.003;
+  return fee;
+}
+
+function calculateJuridicalCashOutFee(amount) {
+  const fee = amount * 0.003;
+  return fee < 0.5 ? 0.5 : fee;
 }
 
 module.exports = calculateFees;
--- src/input.js
+++ src/input.js
@@ -1,12 +1,14 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function getInput() {
   const fileName = process.argv[2];
   if (!fileName) {
-    throw new Error('Please run the program with `node app.js <path>`, and provide a path to a file with input data');
+    throw new Error(
+      "Please run the program with `node app.js <path>`, and provide a path to a file with input data"
+    );
   }
   try {
-    const rawData = fs.readFileSync(fileName, 'utf8');
+    const rawData = fs.readFileSync(fileName, "utf8");
     const data = JSON.parse(rawData);
     return data;
   } catch (err) {
