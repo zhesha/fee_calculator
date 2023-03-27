1a40edb2e9522d344a155f63db0a86e6b043db67
zhesha <savzhe@gmail.com>
2023-03-27 14:39:48 +0300

    added result output

--- src/output.js
+++ src/output.js
@@ -1,5 +1,12 @@
-function showFees() {
-  // ...
+function showFees(feesList) {
+  feesList.forEach(element => {
+    const amount = makeRounding(element);
+    console.log(amount.toFixed(2));
+  });
+}
+
+function makeRounding(amount) {
+  return Math.ceil(amount * 100) / 100;
 }
 
 module.exports = showFees;
